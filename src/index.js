ebbdc38431d559e0510338defef3457e9dff624a
akshaykanade <akshayk0782@gmail.com>
2021-07-20 21:35:57 +0530

    :hatching_chick: release: initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './SCSS/style.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render(
+//     <App />,document.getElementById('root')
+// );
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
