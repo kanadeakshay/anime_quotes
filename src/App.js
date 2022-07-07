6725be09252518a49345a029a6dea46e1c8487db
akshay782 <akshayk0782@gmail.com>
2022-07-08 00:14:25 +0530

    :package: update: dependencies

--- src/App.js
+++ src/App.js
@@ -5,26 +5,18 @@ import Home from './components/Home';
 import About from './components/About';
 import Error from './components/Error';
 import NotFound from './components/Not_Found';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App(){
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route path="/about">
-                    <About/>
-                </Route>
-                <Route path="/error">
-                    <Error/>
-                </Route>
-                <Route path="*">
-                    <NotFound/>
-                </Route>
-            </Switch>
-        </Router>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/error" element={<Error/>} />
+                <Route path="*" element={<NotFound/>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
